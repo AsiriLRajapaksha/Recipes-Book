da6dc85a19cc5428537db0f5e1f1ec2dbfa4fc8c
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-09-04 23:03:33 +0530

    first commit

---
+++ app/receipes/receipe-list/receipe-items/receipe-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipe-items',
+  templateUrl: './receipe-items.component.html',
+  styleUrls: ['./receipe-items.component.scss']
+})
+export class ReceipeItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ app/receipes/receipe-list/receipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../receipes.model';
+@Component({
+  selector: 'app-receipe-list',
+  templateUrl: './receipe-list.component.html',
+  styleUrls: ['./receipe-list.component.scss']
+})
+export class ReceipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Receipes','Description test','../recipe.jpg'),
+    new Recipe('A Test Receipes','Description test','../recipe.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
