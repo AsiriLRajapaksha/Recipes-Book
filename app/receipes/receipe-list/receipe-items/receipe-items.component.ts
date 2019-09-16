c9e8b402383bffd4f9287b9eb84cea211d3476af
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2019-09-17 02:31:16 +0530

    six commit

--- app/receipes/receipe-list/receipe-items/receipe-items.component.ts
+++ app/receipes/receipe-list/receipe-items/receipe-items.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../receipes.model';
 
 @Component({
   selector: 'app-receipe-items',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receipe-items.component.scss']
 })
 export class ReceipeItemsComponent implements OnInit {
+ @Input() recipe : Recipe;
 
   constructor() { }
 
